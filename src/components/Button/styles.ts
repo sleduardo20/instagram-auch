c6506df0aeacfa8d01e89a69f29dfe91c625f577
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-07 09:32:34 -0300

    (styles)🎨: add styles Button component

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,3 +1,25 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 3.2rem;
+
+  border-radius: 0.4rem;
+  background: #0095f6;
+  opacity: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    font-weight: 600;
+    color: #ffff;
+  }
+
+  button {
+    width: 100%;
+    cursor: pointer;
+    background: none;
+    padding: 0.4rem;
+  }
+`;
