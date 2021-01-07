0dbffe41f7fabf7044db074b71542f38cb0fb9ea
sleduardo20 <sl.eduardo20@gmail.com>
2021-01-06 22:22:38 -0300

    (styles)🎨: initial styles page SignIn

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -2,13 +2,42 @@ import styled from 'styled-components';
 
 export const Container = styled.div``;
 
-export const Main = styled.main``;
+export const Main = styled.main`
+  max-width: 93.5rem;
+  margin: 6rem auto 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-export const TopLeft = styled.div``;
+export const TopLeft = styled.div`
+  max-width: 45.4rem;
 
-export const TopRigth = styled.div``;
+  img {
+    width: 100%;
+  }
+`;
 
-export const Login = styled.div``;
+export const TopRigth = styled.div`
+  max-width: 35rem;
+  height: 100%;
+
+  img {
+    width: 17.5rem;
+  }
+`;
+
+export const Login = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    width: 30rem;
+  }
+`;
 
 export const Register = styled.div``;
 
